1099bb46f10c27b223ca7dabc4005094edfbb543
Emmanuel Mawuli <gohemmanuel7@gmail.com>
2025-10-05 00:12:43 +0000

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -1,7 +1,8 @@
 function checkAnswer() {
     // Function body
     const correctAnswer = "4";
-    const userAnswer = document.querySelector("quiz").value;
+    const userAnswer = document.querySelector('[name="quiz"]:checked');
+    const feedback = document.getElementById("feedback");
     if (userAnswer === correctAnswer) {
         feedback.textContent = "Correct! Well done.";
     }
